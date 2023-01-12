2ee32962cd15507cbe7015dc72129524a7e5e7ee
Александр Востриков <aptimict11@gmail.com>
2023-01-12 04:01:38 +0300

    Верстка OnbordingViewController через сториборд

---
+++ MyWeatherApp/View/Onboarding/OnbordingViewController.swift
@@ -0,0 +1,27 @@
+//
+//
+// OnbordingViewController.swift
+// MyWeatherApp
+//
+// Created by Александр Востриков
+//
+    
+
+import UIKit
+
+class OnbordingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+//    Запрос разрешения на использование геопозиции пользователя
+    @IBAction func locationButtonAction(_ sender: Any) {
+        
+    }
+    @IBAction func choiceButtonAction(_ sender: Any) {
+        
+    }
+    
+}
