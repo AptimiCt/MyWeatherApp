01a243d0db8444b5affc93e450195429fed32459
Александр Востриков <aptimict11@gmail.com>
2023-01-14 20:31:01 +0300

    Добавление медода запроса разрешения на использование геолокации.

--- MyWeatherApp/View/Onboarding/OnbordingViewController.swift
+++ MyWeatherApp/View/Onboarding/OnbordingViewController.swift
@@ -8,20 +8,33 @@
     
 
 import UIKit
+import CoreLocation
 
 class OnbordingViewController: UIViewController {
+    
+    let cl = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
     }
     
 //    Запрос разрешения на использование геопозиции пользователя
     @IBAction func locationButtonAction(_ sender: Any) {
-        
+        CheckPermition()
     }
     @IBAction func choiceButtonAction(_ sender: Any) {
         
     }
     
+    private func CheckPermition() {
+        switch cl.authorizationStatus {
+            case .authorizedAlways, .authorizedWhenInUse:
+                cl.startUpdatingLocation()
+            case .denied, .restricted, .notDetermined:
+                cl.requestWhenInUseAuthorization()
+                cl.startUpdatingLocation()
+            @unknown default:
+                print("Не известный статус авторизации.")
+        }
+    }
 }
